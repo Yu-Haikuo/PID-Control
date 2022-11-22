6b3ca0d2c6d32e7cd7b72b18c903e6d17777310d
Yu Haikuo <e0376983@u.nus.edu>
2022-11-23 00:29:42 +0800

    Upload all existing files.

---
+++ a553x_pid_ws/build/a553x_pid/CMakeFiles/a553x_pid.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/mallab/a553x_pid_ws/devel/lib/a553x_pid/a553x_pid"
+  "/home/mallab/a553x_pid_ws/devel/lib/a553x_pid/a553x_pid.pdb"
+  "CMakeFiles/a553x_pid.dir/src/BotControl.cpp.o"
+  "CMakeFiles/a553x_pid.dir/src/pid_control_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a553x_pid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ a553x_pid_ws/build/a553x_pid/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mallab/a553x_pid_ws/src/a553x_pid
+# Build directory: /home/mallab/a553x_pid_ws/build/a553x_pid
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
